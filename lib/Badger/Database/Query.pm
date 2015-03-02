0e9f31604e8b19f22dd341d0e8463d979b3f418e
Andy Wardley <abw@wardley.org>
2015-03-02 16:37:13 +0000

    Cleaned up prepare_sth() and added some basic docs

--- lib/Badger/Database/Query.pm
+++ lib/Badger/Database/Query.pm
@@ -26,45 +26,55 @@ sub init {
     return $self;
 }
 
-
-sub sth {
-    my $self = shift;
-    return $self->{ sth } 
-       ||= $self->{ engine }->prepare( $self->sql );
-}
-
+#-----------------------------------------------------------------------------
+# SQL methods
+#-----------------------------------------------------------------------------
 
 sub sql {
     my $self = shift;
-    return $self->{ sql } 
+    return $self->{ sql }
        ||= $self->prepare_sql;
 }
 
-
 sub prepare_sql {
     # hook for future expansion where SQL is generated on demand
-    shift->todo;
+    shift->not_implemeted('in base class');
 }
 
 
-sub prepare {
+#-----------------------------------------------------------------------------
+# SQL -> STH
+#-----------------------------------------------------------------------------
+
+sub sth {
     my $self = shift;
-    $self->debug('Preparing query: ', $self->sql) if DEBUG;
-    return ($self->{ sth } = $self->{ engine }->prepare( $self->sql, @_ ));
+    return $self->{ sth }
+       ||= $self->prepare_sth;
 }
 
+sub prepare_sth {
+    my $self = shift;
+    return $self->{ engine }->prepare(
+        $self->sql,
+        @_
+    );
+}
+
+
+#-----------------------------------------------------------------------------
+# Query execution and results
+#-----------------------------------------------------------------------------
 
 sub execute {
     my $self = shift;
     $self->debug("Executing query via engine: $self->{ engine } : \n", $self->sql) if DEBUG;
-
-# This was where the bug was - we bypass the reconnection code in query()
-#    $self->{ engine }->execute( $self->sth, @_ );
     $self->debug("execute args: [", join(', ', map { defined $_ ? $_ : '<undef>' } @_), "]\n") if DEBUG;
 
+    # NOTE: we must call execute_query() rather than execute() because the
+    # MySQL engine subclass (Badger::Database::Engine::MySQL) defines some
+    # additional magic to work around the "Server has gone away" problem.
     $self->{ engine }->execute_query( $self, @_ );
 }
-
 
 sub row {
     my $self = shift;
@@ -73,11 +83,10 @@ sub row {
     $self->debug("query row() fetched ", $sth->rows, " row(s)\n") if DEBUG;
 
     return  $sth->fetchrow_hashref()
-        || ($sth->err 
+        || ($sth->err
             ? $self->error_msg( dbi => fetchrow_hashref => $sth->errstr )
             : undef);
 }
-
 
 sub rows {
     my $self = shift;
@@ -86,21 +95,20 @@ sub rows {
     $self->debug("rows() fetched ", $sth->rows, " row(s)\n") if DEBUG;
 
     return $sth->fetchall_arrayref({ })
-        || ($sth->err 
+        || ($sth->err
             ? $self->error_msg( dbi => fetchall_arrayref => $sth->errstr)
             : undef );
 }
-
 
 sub column {
     my $self = shift;
     my $sth  = $self->execute(@_);
     my (@values, $value);
 
-    $sth->bind_col(1, \$value) 
+    $sth->bind_col(1, \$value)
         || return $self->error_msg( dbi => bind_param => $sth->errstr );
 
-    push(@values, $value) 
+    push(@values, $value)
         while $sth->fetch;
 
     $self->debug("column() fetched ", scalar(@values), " value(s)\n") if DEBUG;
@@ -111,6 +119,11 @@ sub column {
 }
 
 
+
+#-----------------------------------------------------------------------------
+# Cleanup methods
+#-----------------------------------------------------------------------------
+
 sub destroy {
     my $self = shift;
     $self->debug("destroying query") if DEBUG;
@@ -120,4 +133,76 @@ sub destroy {
 
 1;
 
+=head1 NAME
 
+Badger::Database::Query - SQL query object base class
+
+=head1 DESCRIPTION
+
+This module defines a base class for object that generate SQL queries
+programmatically.
+
+=head1 METHODS
+
+The following methods are defined in addition to those inherited from the
+L<Cog::Query::Scheme:Search>, L<Contentity::Database::Search>,
+L<Badger::Database::Query::Select>, L<Badger::Database::Query>,
+L<Badger::Database::Base>, L<Badger::Base> and L<Cog::Base> classes.
+
+=head2 sql()
+
+Returns the C<sql> query specified as a configuration option, or
+calls the L<prepare_sql()> method to generate the SQL on demand.
+
+=head2 prepare_sql()
+
+This method should be redefined by subclasses that generate the SQL query
+dynamically.  In this base class module it throws an error to warn the
+developer that it hasn't been redefined.
+
+=head2 sth()
+
+Returns a L<DBI> statement handle for the SQL query returned by the
+L<sql()> method.  It calls the L<prepare_sth()> method once and then caches
+the result for any future invocations.
+
+=head2 prepare_sth()
+
+Internal method called by L<sth()> to prepare a statement handle for the
+SQL query.
+
+=head2 execute(@args)
+
+Internal method called by the L<row()>, L<rows()> and L<column()> methods
+to execute the query statement handle that is returned by the L<sth()>
+method.
+
+=head2 row(@args)
+
+Returns the first row in the results returned by calling the
+L<execute()> method.
+
+=head2 rows(@args)
+
+Returns all rows in the results returned by calling the
+L<execute()> method.
+
+=head2 column(@args)
+
+Returns a single column of values returned by calling L<execute()>.
+
+=head1 AUTHOR
+
+Andy Wardley E<lt>abw@wardley.orgE<gt>.
+
+=head1 COPYRIGHT
+
+Copyright (C) 2008-2014 Andy Wardley.  All Rights Reserved.
+
+=head1 SEE ALSO
+
+L<Badger::Database::Query::Select>,
+L<Badger::Database::Base>,
+L<Badger::Base>.
+
+=cut
